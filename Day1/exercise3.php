75deebc82ba5b04fd3bfdca91f6d7a4928dc0784
harishwar1201 <87687891+harishwar1201@users.noreply.github.com>
2021-08-17 19:02:39 +0530

    Solutions

--- Day1/exercise3.php
+++ Day1/exercise3.php
@@ -1,17 +1,23 @@
 <?php
+function camelcase($input_string)
+{
+    $temp_string = explode("_", $input_string);
+    $converted_string = $temp_string[0];
+    for ($index = 1; $index < count($temp_string); $index++) {
+        $converted_string = $converted_string . ucfirst($temp_string[$index]);
+    }
+    return $converted_string;
+}
+
 function convert($input_array)
 {
     $result = array();
-    foreach ($input_array as $item) {
-        $temp_string = explode("_", $item);
-        $converted_string = $temp_string[0];
-        for ($index = 1; $index < count($temp_string); $index++) {
-            $converted_string = $converted_string . ucfirst($temp_string[$index]);
-        }
-        array_push($result, $converted_string);
+    foreach($input_array as $value){
+        array_push($result,camelcase($value));
     }
     return $result;
 }
 
 $input_array = array("snake_case_example", "camel_case_example");
+
 print_r(convert($input_array));
